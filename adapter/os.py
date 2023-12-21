a851306337119398c0a9bef2e7e77bc4fba2355c
Yukio Nozawa <personal@nyanchangames.com>
2023-12-22 00:04:32 +0900

    Add test for os operations

--- adapter/os.py
+++ adapter/os.py
@@ -3,6 +3,15 @@ Provides a class for various OS-specific operations.
 I'm wrapping these functions here so that I can mock them in tests.
 """
 
+import os
+import subprocess
+
 class OSOperation:
     def fileExists(self, path):
         return os.path.exists(path)
+
+    def open(self, path, mode):
+        return open(path, mode)
+
+    def popen(self, args, **kwargs):
+        return subprocess.Popen(args, **kwargs)
