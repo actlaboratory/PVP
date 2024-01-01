02220fb30bce1b1b8e763cb1bc1c25c1e2a83613
Yukio Nozawa <personal@nyanchangames.com>
2024-01-01 14:50:39 +0900

    Bump to 1.0.0!!!!!!!!!!!!!!

--- constants.py
+++ constants.py
@@ -8,9 +8,9 @@ import wx
 APP_FULL_NAME = "Parametric Video Processor"#アプリケーションの完全な名前
 APP_NAME="PVP"#アプリケーションの名前
 APP_ICON = None
-APP_VERSION="0.0.1"
-APP_LAST_RELEASE_DATE="9999-99-99"
-APP_COPYRIGHT_YEAR="2023"
+APP_VERSION="1.0.0"
+APP_LAST_RELEASE_DATE="2024-01-01"
+APP_COPYRIGHT_YEAR="2023-2024"
 APP_LICENSE="Apache License 2.0"
 APP_DEVELOPERS="Yukio Nozawa"
 APP_DEVELOPERS_URL="https://actlab.org/"
